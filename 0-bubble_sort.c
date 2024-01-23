b6869add643c22e544755a667c3dd60f75dc178a
baragu <baraguantonyy@gmail.com>
2024-01-23 10:09:23 +0300

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+* bubble_sort - Sort an array of integers in ascending order
+* @array: The array to be sorted
+* @size: The number of elements in @array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
