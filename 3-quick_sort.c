d3c1eda2082506ae52a5ec65589e35ee63700b89
EUGENE <eugenewestley95@gmail.com>
2024-01-23 12:57:40 -0800

    ten

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 void swap(int *a, int *b)
@@ -15,21 +13,23 @@ void lomuto_partition(int *array, int low, int high, size_t size, int *i)
     {
         int pivot = array[high];
         int j = low - 1;
+        int k = low;
 
-        for (int k = low; k <= high - 1; k++)
+        while (k <= high - 1)
         {
             if (array[k] <= pivot)
             {
                 j++;
                 swap(&array[j], &array[k]);
-                if (j != k) // Add this condition to avoid unnecessary prints
+                if (j != k) /* This avoids unnecessary prints*/
                     print_array(array, size);
             }
+            k++;
         }
 
         swap(&array[j + 1], &array[high]);
-        if ((j + 1) != high) // Add this condition to avoid unnecessary prints
-            print_array(array, size);
+        if ((j + 1) != high)
+		print_array(array, size);
 
         *i = j + 1;
     }
