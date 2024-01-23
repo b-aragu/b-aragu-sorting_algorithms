34ad6a34dcf05cfc8f1f2e7c6d2a0833bf74ecb7
EUGENE <eugenewestley95@gmail.com>
2024-01-23 10:33:30 -0800

    seven

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,24 +1,21 @@
 #include "sort.h"
 
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void selection_sort(int *array, size_t size)
-{
+void selection_sort(int *array, size_t size) {
     size_t i, j, min_idx;
 
-    for (i = 0; i < size-1; i++)
-    {
+    for (i = 0; i < size - 1; i++) {
         min_idx = i;
-        for (j = i+1; j < size; j++)
-          if (array[j] < array[min_idx])
-            min_idx = j;
-
-        swap(&array[min_idx], &array[i]);
-        print_array(array, size);
+        for (j = i + 1; j < size; j++) {
+            if (array[j] < array[min_idx]) {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i) {
+            int temp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = temp;
+            print_array(array, size);
+            printf("\n");
+        }
     }
 }
